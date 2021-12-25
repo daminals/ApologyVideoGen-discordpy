35f3a1aba04c42104d119acf86e871cf023311a0
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2021-12-24 21:30:29 -0500

    seperate script.py

--- main.py
+++ main.py
@@ -6,50 +6,13 @@ import gtts
 from moviepy.editor import *
 from moviepy.audio.fx.volumex import volumex
 import os, random, ffmpy, asyncio
+from script import *
 
 # TODO: add subtitles 🥺😳
 # TODO: Organize different steps into separate functions (ex: tts to tts function)
 # TODO: Make into a website and/or discord bot -- COMPLETED DISC BOT
 # TODO: DEBUG!!! It crashes and is unable to load the video every so often, and that must be fixed
 
-
-apology_intros = ["I made a severe and continuous lapse in my judgement, and I don’t expect to be forgiven. I’m "
-                  "simply here to apologize. ", "I want to talk to you guys about some mistakes I have made, ",
-                  "yeah, I wasn't acting very sexy earlier...   ", "...Long and Deep sigh...",
-                  "Hey crewmates! SORRY for being sus earlier... ", 
-                  ]
-
-bs = ["My waitress at taco bell got my order wrong three months ago", "I couldn't find my right sock this morning",
-      "My mommy took away my PS vita", "My sugar daddy stopped funding my twizzler addiction",
-      "I scratched my iPhone screen", "I was tried for first degree murder", "I was born a scorpio",
-      "I was really hungry at the time"
-      "I broke my iPhone by putting it too close to the cutting board, but it's ok my Dad will just buy me a new one",
-      "My brita filter cracked", 
-      "Mercury was in retrograde",
-      "My stock portfolio dropped two cents",
-      "Bitcoin went down", 
-      ]
-
-middle = ["I don't want your forgiveness, just the open space to be able to say how deeply sorry I am.  ",
-          "The goal of my content is always to entertain, I never meant to let it get out of hand.  ",
-          "The inhumane actions that I have done were truly despicable.  ",
-          "It was once I lost one thousand insta followers that I understood the error of my ways.   "
-          "When I lost my sponsorship with Manscaped, I realized how wrong I was.  "
-          ]
-
-challenge = ["It's just been so hard for me since ",
-             "Life has always been difficult for me, for instance ",
-             "Life for me has been like that tiktok taylor swift audio: Screaming! Crying! ever since ",
-             "I've been feeling so not epic gamer because "
-             ]
-
-wrongness_list = ["is the worst thing I have ever done in my entire life, no contest",
-                  "is the lowest of lows, and it should not have been me",
-                ]
-
-thank_u = ["Thank you everyone for giving me this time to apologize for my actions",
-           "Thanks for listening to this video, follow for more content like this"
-           ]
 
 def gen_ID(char):
     ID = ''
@@ -76,10 +39,7 @@ async def main(bool_inp,ID,apolo=''):
         reason = apolo
     else:
         reason = input('Why are you apologizing? ')
-    Intro = random.choice(apology_intros)
-    Middle_part = f".  I am deeply and truly sorry for {reason.lower()}. It was wrong, disgraceful, and I promise it will never happen again,  .   {reason.lower()} {random.choice(wrongness_list)}. . .  {random.choice(middle)}"
-    Conclusion = f' {random.choice(thank_u)}. {random.choice(challenge)} {random.choice(bs)}. I love each of you guys so   so much. Thank you, and Don\'t forget to SMASH that like button and subscribe for more content!       .    '
-    script = Intro + Middle_part + Conclusion
+    script = create_script(reason)
     print('Processing audio...')
     
     #ttsEngine = pyttsx3.init()
